b99434fe976d75a99ec97dac4d429735a8aafa1a
Bernico Constantino <bernico.constantino15@gmail.com>
2023-02-03 21:31:02 +0700

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,14 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
-    public function register() {
+    public function register(){
         return view('profile.register');
     }
 
-    public function login() {
+    public function postregister(Request $request){
+        $data = $request->validate([
+            'firstname' => 'required|max:25',
+            'lastname' => 'required|max:25',
+            'email' => 'required|email:dns',
+            'role' => 'required',
+            'gender' => 'required',
+            'picture' => 'required',
+            'password' => 'required|min:8',
+            'confirmpassword' => 'required|min:8'
+        ]);
+
+        $file = $request->file('picture');
+        $imageName = time().'.'.$file->getClientOriginalExtension();
+        Storage::putFileAs('public/images', $file, $imageName);
+        $imageName = 'images/'.$imageName;
+
+        $data['password'] = bcrypt($data['password']);
+
+        return redirect('/login');
+    }
+
+    public function login(){
         return view('profile.login');
+    }
+
+    public function postlogin(Request $request){
+        $data = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required|min:8'
+        ]);
+
+        if(Auth::attempt($data)){
+            $request->session()->regenerate();
+            return redirect()->intended('/home');
+        }
+
+        return back()->with('error', 'Email atau Password salah');
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,5 +13,29 @@
             </a>
         </div>
     </header>
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col">
+                <a href="/home" class="nav-link px-2 fs-5 link-dark">Home</a>
+            </div>
+            <div class="col">
+                <a href="/cart" class="nav-link px-2 fs-5 link-dark">Cart</a>
+            </div>
+            <div class="col">
+                <a href="/profile" class="nav-link px-2 fs-5 link-dark">Profile</a>
+            </div>
+        </div>
+    </div>
 </div>
+
+{{-- <div class="container">
+    <div class="row">
+        <div class="col-3 d-flex justify-content-center mb-3 mt-2">
+            <div class="card">
+                <img src="" alt="">
+                <a href="/home" class="btn btn-primary mt-auto">More Detail</a>
+            </div>
+        </div>
+    </div>
+</div> --}}
 @endsection
